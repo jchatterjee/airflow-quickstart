a8cf6a64f73e5d3ef0be0eafcc70b1b1f4503986
jchatterjee <chatterjee.prime@gmail.com>
2023-06-21 16:36:55 -0400

    Update user_input_variables.py

--- include/global_variables/user_input_variables.py
+++ include/global_variables/user_input_variables.py
@@ -2,7 +2,7 @@
 # Enter your own info! #
 # -------------------- #
 
-MY_NAME = "Friend"
-MY_CITY = "Bern"
-HOT_DAY = 30 # modify what you consider a hot day in degree Celsius
-BIRTH_YEAR = 1990
+MY_NAME = "Joydeep"
+MY_CITY = "Exeter"
+HOT_DAY = 40 # modify what you consider a hot day in degree Celsius
+BIRTH_YEAR = 1984
